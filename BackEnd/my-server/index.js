1783e27ed8d435a0a5c6b91512a0503ccdee1eeb
Kashish Santuka <kashusantuka11@gmail.com>
2024-11-09 16:25:39 +0530

    Second Commit

--- BackEnd/my-server/index.js
+++ BackEnd/my-server/index.js
@@ -7,16 +7,39 @@ import createRoutes from "./routes/Create.js";
 const app = express();
 app.use(express.json());
 
-const corsOption = {
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  credentials: true,
-};
+// const corsOption = {
+//   origin: "http://localhost:5173",
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//   credentials: true,
+// };
 // app.use((err, req, res, next) => {
 //   const status = err.statusCode || 500;
 //   const message = err.message || "An unexpected error occurred.";
 //   res.status(status).json({ message });
 // });
+
+const allowedOrigins = [
+  "https://workforcepanel.netlify.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(cors(corsOption));
 app.use("/users", userRoutes);
