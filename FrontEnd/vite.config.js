b18f06b467be8b8e376536527a5863238504a9c0
Kashish Santuka <139388471+KashishSantuka@users.noreply.github.com>
2024-11-16 18:11:32 +0530

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Ensures relative paths are used for assets
+  build: {
+    outDir: 'dist', // Specifies output directory
+    emptyOutDir: true, // Clears the output directory before building
+  },
+  server: {
+    port: 3000, // Development server port
+  },
+});
